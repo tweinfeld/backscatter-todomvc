8b0afb2517e667bd2261b02d57a75acb71070a22
Tal Weinfeld <talw@webwise.co.il>
2015-09-27 18:57:17 +0300

    TodoMVC Conformity: Trimming at source, removing empty renames

--- js/controller/application.js
+++ js/controller/application.js
@@ -5,28 +5,28 @@ import SessionModel from '../model/session'
 
 export default (window) => {
 
-	const trim = (text) => (text.match(/^\s*(.+?)\s*$/) || [""]).pop();
+    let session = new SessionModel;
+    session.get('tasks').set(JSON.parse(window.localStorage["todo-backscatter"] || "[]"));
 
-	let session = new SessionModel;
-	session.get('tasks').set(JSON.parse(window.localStorage["todo-backscatter"] || "[]"));
+    let render = ()=> {
+        React.render(React.createElement(Main, {
+            tasks: session.get('tasks').toJSON(),
+            filter: session.get('filter'),
+            onAllTasksToggleCompleted: () => session.get('tasks').toggleCompleted(),
+            onTaskToggleCompleted: (taskId) => session.get('tasks').get(taskId).toggleCompleted(),
+            onTaskRename: (taskId, newDescription) => session.get('tasks').get(taskId).set({description: newDescription}, {validate: true}),
+            onFilterChange: (filter) => session.set({filter}),
+            onTaskRemove: (taskId) => session.get('tasks').remove(session.get('tasks').get(taskId)),
+            onTaskAdd: (description) => {
+                session.get('tasks').add({id: _.uniqueId('task-'), description: description}, {validate: true})
+            },
+            onAllCompletedTasksRemove: ()=> session.get('tasks').removeCompleted()
+        }), document.getElementsByTagName('main')[0]);
+    };
 
-	let render = ()=>{
-		React.render(React.createElement(Main, {
-			tasks: session.get('tasks').toJSON(),
-			filter: session.get('filter'),
-			onAllTasksToggleCompleted: () => session.get('tasks').toggleCompleted(),
-			onTaskToggleCompleted: (taskId) => session.get('tasks').get(taskId).toggleCompleted(),
-			onTaskRename: (taskId, newDescription) => session.get('tasks').get(taskId).set({ description: trim(newDescription) }, { validate: true }),
-			onFilterChange: (filter) => session.set({ filter }),
-			onTaskRemove: (taskId) => session.get('tasks').remove(session.get('tasks').get(taskId)),
-			onTaskAdd: (description) => { session.get('tasks').add({ id: _.uniqueId('task-'), description: trim(description) }, { validate: true }) },
-			onAllCompletedTasksRemove: ()=> session.get('tasks').removeCompleted()
-		}), document.getElementsByTagName('main')[0]);
-	};
+    let persist = _.debounce(()=> window.localStorage["todo-backscatter"] = JSON.stringify(session.get('tasks').toJSON()), 500);
 
-	let persist = _.debounce(()=> window.localStorage["todo-backscatter"] = JSON.stringify(session.get('tasks').toJSON()), 500);
-
-	// The following line is the one and only usage of backscatter across this entire application
-	// It will make sure "render" is triggered everytime something changes anywhere below "session"
-	session.backscatterOn(_.compose(persist, _.debounce(render)))();
+    // The following line is the one and only usage of Backscatter across this entire application
+    // It will make sure "render" is triggered anytime some model/collection changes underneath "session"
+    session.backscatterOn(_.compose(persist, _.debounce(render)))();
 }
